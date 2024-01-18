daca7f458206a69ef21d59f9c1714ae484adf391
Juri Sacchetta <jurisacchetta@gmail.com>
2024-01-18 14:05:16 +0100

    Add pseudo-random behavior to the scheduler

--- fibers/fibers.c
+++ fibers/fibers.c
@@ -1,17 +1,26 @@
 #include "qemu/osdep.h"
-#include "src/fibers-thread.h"
 #include "pth/pth.h"
+#include "unistd.h"
 
 #include "fibers.h"
+#include "src/fibers-thread.h"
+#include "src/fibers-futex.h"
+#include "src/fibers-utils.h"
 
 void fibers_init(CPUArchState *cpu)
 {
+   srandom(2000);
+   fibers_futex_init();
    fibers_thread_init(cpu);
 }
 
 void fibers_call_scheduler(void)
 {
    if (fibers_count - BASE_FIBERS_TID > 1) {
-      pth_yield(NULL);
+      int random_number = random() % 2;
+      if(random_number < 1) {
+         FIBERS_LOG_DEBUG("Calling scheduler num: %d\n", random_number);
+         pth_yield(NULL);
+      }
    }
 }
