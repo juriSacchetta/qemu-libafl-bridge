2f65d720d81f46352d0400e81a45dabe30cff1ff
Juri Sacchetta <jurisacchetta@gmail.com>
2024-01-29 15:23:54 +0100

    Removed conditional scheduler call; logic moved into LibAFL internals

--- fibers/fibers.c
+++ fibers/fibers.c
@@ -6,15 +6,6 @@
 #include "src/fibers-futex.h"
 #include "src/fibers-utils.h"
 
-static uint32_t xorshift_state = 123456789;
-
-static uint32_t xorshift32(void) {
-    xorshift_state ^= (xorshift_state << 13);
-    xorshift_state ^= (xorshift_state >> 17);
-    xorshift_state ^= (xorshift_state << 5);
-    return xorshift_state;
-}
-
 void fibers_init(CPUArchState *cpu)
 {
    fibers_futex_init();
@@ -25,11 +16,8 @@ void fibers_call_scheduler(void)
 {
    int available_threads = pth_ctrl(PTH_CTRL_GETTHREADS_NEW | PTH_CTRL_GETTHREADS_READY | PTH_CTRL_GETTHREADS_SUSPENDED);
    if (available_threads > 0) {
-      int random_number = xorshift32() % 2;
-      if(random_number < 1) {
-         FIBERS_LOG_DEBUG("Calling scheduler num: %d\n", random_number);
-         pth_yield(NULL);
-      }
+      FIBERS_LOG_DEBUG("Calling scheduler num: %d\n", random_number);
+      pth_yield(NULL);
    }
 }
 
